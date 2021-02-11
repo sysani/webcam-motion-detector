2dc39a3b8e6d33edc3d3e9ef7c0f86e1fcd77ccd
sysani <russo.anitaa@gmail.com>
2021-02-11 17:54:28 -0500

    object detection pt 2

--- motion_detector.py
+++ motion_detector.py
@@ -16,9 +16,19 @@ while True:
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
+    (cntrs,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cntrs:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+
+
     cv2.imshow("Capturing", gray)
     cv2.imshow("Delta", delta_frame)
     cv2.imshow("Threshold", thresh_frame)
+    cv2.imshow("Colour Frame", frame)
 
     key = cv2.waitKey(1000)
     if key == ord('q'):
