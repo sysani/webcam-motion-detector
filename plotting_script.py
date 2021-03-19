f39e69b7d5dcc5b5fce501ec5d8f3c7683ff7718
sysani <russo.anitaa@gmail.com>
2021-03-19 13:04:44 -0400

    updating plotting script to include facial recognition

--- plotting_script.py
+++ plotting_script.py
@@ -1,11 +1,12 @@
-from motion_detector import motion_df
+from motion_detector import motion_df, face_df
 from bokeh.plotting import show, figure, output_file
 
-p = figure(x_axis_type="datetime", height=100, width=500, title="Motion Detection Times")
+p = figure(x_axis_type="datetime", height=200, width=500, title="Motion Detection Times")
 p.yaxis.minor_tick_line_color=None
 p.ygrid[0].ticker.desired_num_ticks=1
 
-q = p.quad(left=motion_df['Start'],right=motion_df['End'],bottom=0,top=1,color="green")
+q1 = p.quad(left=motion_df['Start'],right=motion_df['End'],bottom=0,top=1,color="yellow")
+q1 = p.quad(left=face_df['Start'],right=face_df['End'],bottom=0,top=1,color="green")
 
 output_file("graph.html")
 show(p)
