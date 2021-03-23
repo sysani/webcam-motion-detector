e8cf4efe200a7813fba1e96400b6f98f3c4f9731
sysani <russo.anitaa@gmail.com>
2021-03-23 14:48:58 -0400

    updating graph to show time info on hover

--- plotting_script.py
+++ plotting_script.py
@@ -1,12 +1,28 @@
 from motion_detector import motion_df, face_df
 from bokeh.plotting import show, figure, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+
+motion_df['Start_Str'] = motion_df['Start'].dt.strftime('%Y-%m-%d %H:%M:%S')
+motion_df['End_Str'] = motion_df['End'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+face_df['Start_Str'] = face_df['Start'].dt.strftime('%Y-%m-%d %H:%M:%S')
+face_df['End_Str'] = face_df['End'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+cds = ColumnDataSource(motion_df)
+cds_f = ColumnDataSource(face_df)
 
 p = figure(x_axis_type="datetime", height=200, width=500, title="Motion Detection Times")
 p.yaxis.minor_tick_line_color=None
 p.ygrid[0].ticker.desired_num_ticks=1
 
-q1 = p.quad(left=motion_df['Start'],right=motion_df['End'],bottom=0,top=1,color="yellow")
-q1 = p.quad(left=face_df['Start'],right=face_df['End'],bottom=0,top=1,color="green")
+hover_motion = HoverTool(tooltips=[("Start", "@Start_Str"), ("End", "@End_Str")])
+p.add_tools(hover_motion)
+
+hover_face = HoverTool(tooltips=[("Start", "@Start_Str"), ("End", "@End_Str")])
+p.add_tools(hover_face)
+
+q1 = p.quad(left='Start',right='End',bottom=0,top=1,color="yellow",source=cds)
+q1 = p.quad(left='Start',right='End',bottom=0,top=1,color="green",source=cds_f)
 
 output_file("graph.html")
 show(p)
